e9ec5f13eeb17985908ae9951cfc1a338789180b
Alexander Silva <olman.silva@correounivalle.edu.co>
2024-10-05 11:51:46 -0500

    Pequena actualizacion controller y fxml instructions

--- src/main/java/com/example/sudoku/controllers/InstructionsController.java
+++ src/main/java/com/example/sudoku/controllers/InstructionsController.java
@@ -8,6 +8,7 @@ import com.example.sudoku.views.GameView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -16,9 +17,13 @@ public class InstructionsController {
     public TextArea textAreaInstructions;
 
     @FXML
-    public void onActiontextAreaInstructions(MouseEvent mouseEvent) {
-
-        //TextArea.setEditable(false);
+    public void onActionTextAreaInstructions(MouseEvent mouseEvent) {
         System.out.println("MouseEvent");
+        textAreaInstructions.setEditable(false);
+    }
+
+    public void onKeyPressedTextAreaInstructions(KeyEvent keyEvent) {
+        System.out.println("KeyEvent");
+        textAreaInstructions.setEditable(false);
     }
 }
